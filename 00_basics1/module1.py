72e13fcd7f4b085db77123ebbfc2f69cd5d6b229
Sourav Kamboj <souravkambojj@gmail.com>
2019-12-15 18:06:17 +0530

    Add files via upload

---
+++ 00_basics1/module1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+if __name__=='__main__':
+    print("hii i m module 1 running as: ",__name__)
+    
+else:
+    print("hii i m module 1 runing as:",__name__)
+
+
+# In[ ]:
+
+
+
+
